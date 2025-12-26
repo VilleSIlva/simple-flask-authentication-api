74f289062318ed29df71b6389891a3054e320d3d
VilleSilva <villenunescode@gmail.com>
2025-12-26 16:12:12 -0300

    Rota de login

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from database import db
 from models.User import User
+from flask_login import  LoginManager, login_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
 
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+login_manager.login_view = "login"
 
 db.init_app(app)
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login",methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+       user = User.query.filter_by(username=username).first()
+
+       if user and user.password == password:
+            login_user(user)
+            return jsonify({"message":"Login Realizado com sucesso"}),400
+
+
+    return jsonify({"message":"Credenciais Inválidas"}),400
+
 
 
 if __name__ == "__main__":
