2698a9e1901d71a28d3ecc51a5eca80788c4b492
VilleSilva <villenunescode@gmail.com>
2025-12-26 15:15:31 -0300

    Configurando o flask e flask-sqlalchemy

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from database import db
+from models.User import User
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db.init_app(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
